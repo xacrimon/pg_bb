5cb86aaaf06d2986d5a5ccbf3966b56d755ab581
Joel Wejdenstål <jwejdenstal@icloud.com>
2025-01-22 08:58:12 +0100

    backup pipeline optimizations

--- src/create_backup.rs
+++ src/create_backup.rs
@@ -9,6 +9,7 @@ use std::fs::{self, File};
 use std::io;
 use std::io::{Read, Write};
 use std::process::{Command, Stdio};
+use std::str;
 use std::sync::mpsc::{self, channel, TryRecvError};
 use std::thread;
 use std::time;
@@ -82,7 +83,7 @@ pub fn run(ctx: &Context, opts: &Options) -> Result<()> {
     let target_file = File::create(&backup_target_path)?;
     info!("writing backup to {:?}...", backup_target_path);
 
-    let buffer_and_stream = backup_buffered.as_slice().chain(backup_stream);
+    let buffer_and_stream = backup_buffered.as_slice().chain(backup_stream.into_inner());
     let total_read_bytes = Cell::new(0);
     let total_written_bytes = Cell::new(0);
 
@@ -95,21 +96,19 @@ pub fn run(ctx: &Context, opts: &Options) -> Result<()> {
     let start_time = time::Instant::now();
     let mut last_info = start_time;
 
-    let read = || total_read_bytes.get() / MIB_UNIT_SCALE;
-    let read_rate = || (read() as f32) / start_time.elapsed().as_secs_f32();
-    let written = || total_written_bytes.get() / MIB_UNIT_SCALE;
-    let written_rate = || (written() as f32) / start_time.elapsed().as_secs_f32();
-    let ratio = || (total_read_bytes.get() as f32) / (total_written_bytes.get() as f32);
-
     let log_stats = |last: bool| {
+        let elapsed = start_time.elapsed().as_secs_f32();
+        let read = total_read_bytes.get() / MIB_UNIT_SCALE;
+        let written = total_written_bytes.get() / MIB_UNIT_SCALE;
+
         info!(
-            "{}processed {} MiB @ {:.0} MiB/s, written {} MiB @ {:.0} MiB/s, compression ratio: {:.2}x",
-            if !last { "progress: " } else { "" },
-            read(),
-            read_rate(),
-            written(),
-            written_rate(),
-            ratio()
+            "{header}processed {read} MiB @ {read_rate:.0} MiB/s, written {written} MiB @ {written_rate:.0} MiB/s, compression ratio: {ratio:.2}x",
+            header = if !last { "progress: " } else { "" },
+            read = read,
+            read_rate = read as f32 / elapsed,
+            written = written,
+            written_rate = written as f32 / elapsed,
+            ratio = total_read_bytes.get() as f32 / total_written_bytes.get() as f32
         );
     };
 
@@ -138,23 +137,33 @@ pub fn run(ctx: &Context, opts: &Options) -> Result<()> {
 
 fn find_wal_label(stream: SplitReceiver) -> Result<String> {
     let mut archive = tar::Archive::new(stream);
+    let mut buffer = Vec::new();
 
     for entry in archive.entries()? {
         let mut entry = entry?;
-        if entry.path()?.to_str() == Some("backup_label") {
-            let mut contents = String::new();
-            entry.read_to_string(&mut contents)?;
+        if entry.path()?.to_str() != Some("backup_label") {
+            continue;
+        }
 
-            for line in contents.lines() {
-                if line.starts_with("START WAL LOCATION") {
-                    let parts: Vec<&str> = line.split("file").collect();
-                    if let Some(part) = parts.get(1) {
-                        if part.len() >= 1 {
-                            return Ok(part[1..part.len() - 1].to_string());
-                        }
-                    }
-                }
+        buffer.clear();
+        let len = entry.read_to_end(&mut buffer)?;
+        let contents = str::from_utf8(&buffer[..len])?;
+
+        for line in contents.lines() {
+            if !line.starts_with("START WAL LOCATION") {
+                continue;
             }
+
+            let part = match line.split("file").skip(1).next() {
+                Some(part) => part,
+                None => continue,
+            };
+
+            if part.len() < 2 {
+                continue;
+            }
+
+            return Ok(part[1..part.len() - 1].to_string());
         }
     }
 
@@ -218,6 +227,10 @@ impl<R> Splitter<R> {
     fn new(inner: R) -> (Self, SplitReceiver) {
         let (tx, rx) = channel();
         (Self { inner, tx }, SplitReceiver::new(rx))
+    }
+
+    fn into_inner(self) -> R {
+        self.inner
     }
 }
 
